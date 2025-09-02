aa20fe23bf2b0354c3ffed174bfb0bb7a22707c3
Diego <sdieg82@gmail.com>
2025-09-02 17:02:59 -0500

    moviesProvider getNowPlaying

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,32 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider= StateNotifierProvider<MoviesNotifier, List<Movie>>((ref){
+  
+  final fetchMoreMovies=ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(
+    fetchMoreMovies,
+  );
+});
+  
+
+typedef MovieCallback = Future<List<Movie>> Function ({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>>{
+  
+  int currentPage=0;
+  MovieCallback fecthMoreMovies;
+  
+  MoviesNotifier(
+     this.fecthMoreMovies,
+  ):super([]);
+  Future<void>loadNextPage()async{
+    currentPage ++;
+    final List<Movie> movies= await fecthMoreMovies(page: currentPage);
+    state=[...state,...movies];
+
+  }
+
+}
